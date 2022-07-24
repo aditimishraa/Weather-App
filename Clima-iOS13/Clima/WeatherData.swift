9a0258ced7d235a9766cafae337e377a12cc4d30
aditimishraa <71449873+aditimishraa@users.noreply.github.com>
2022-07-24 10:59:21 +0530

    Add files via upload

---
+++ Clima-iOS13/Clima/WeatherData.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Aditi on 07/08/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    
+    
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+struct Weather: Codable {
+    let description: String
+    let id: Int
+    
+    
+}
